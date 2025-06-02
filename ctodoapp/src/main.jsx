8510fe12c29d7d7b14d9f14005a8e47f42e84407
Irbaz2004 <irbazahmeds20@gmail.com>
2025-06-02 12:42:28 +0530

    change

--- ctodoapp/src/main.jsx
+++ ctodoapp/src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
 import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <StrictMode>
     <App />
-    </HashRouter>
+    </StrictMode>
 );
